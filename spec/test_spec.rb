c276a95592b1375189aaf89c54e8b43c1bb6e248
Yuki Izumi <yuki@kivikakk.ee>
2017-06-28 15:50:55 +1000

    Basic test for the custom renderer

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,8 +1,12 @@
 require "spec_helper"
 
 describe JekyllCommonMarkCustomRenderer do
-  it "works" do
-  end
+  let(:renderer) { JekyllCommonMarkCustomRenderer.new }
+  let(:doc) { CommonMarker.render_doc("# Hello\n\n## Hi, world!") }
+  subject { renderer.render(doc) }
+
+  it { is_expected.to match %r{<h1 id="hello">Hello</h1>} }
+  it { is_expected.to match %r{<h2 id="hi-world">Hi, world!</h2>} }
 end
 
 describe Jekyll::Converters::Markdown::CommonMarkGhPages do
